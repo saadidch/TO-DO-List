71f9cedb55d3f0131b9af31eca2f4dde198106dd
saadidch <saadidrees@windows.com>
2023-01-14 19:50:14 -0500

    Added new components for lists.

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from "../../Todo";
 
 @Component({
   selector: 'app-todos',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+  todos:Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno:1,
+        title:"This is title 1",
+        desc:"Description",
+        active: true
+      },
+      {
+        sno:2,
+        title:"This is title 2",
+        desc:"Description",
+        active: true
+      },{
+        sno:3,
+        title:"This is title 3",
+        desc:"Description",
+        active: true
+      }
+    ]
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
